4204f36922a5e14bbf7d577ff2a8636dcb7d4f5c
bondpw <61185261+bondpw@users.noreply.github.com>
2020-03-16 16:25:58 +0000

    Add files via upload

--- test_functions.py
+++ test_functions.py
@@ -1,1 +1,8 @@
 import pytest as pyt
+import numpy as np
+from ParticleForUse import Particle as Pa 
+
+def test_update(): 
+    assert Pa([1.0,1.0],[1.0,1.0],[1.0,1.0],'TestObject').update(5) == Pa([6.0,6.0],[6.0,6.0],[1.0,1.0],'TestObject')
+    
+    assert Pa([1.0,1.0],[1.0,1.0],[1.0,1.0],'TestObject').update2(5) == Pa([31.0,31.0],[6.0,6.0],[1.0,1.0],'TestObject')
