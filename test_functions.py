c2990d45ac15dd373fe629b7ab4b1ca7f656620d
bondpw <61185261+bondpw@users.noreply.github.com>
2020-03-17 14:19:57 +0000

    Add files via upload

--- test_functions.py
+++ test_functions.py
@@ -1,6 +1,9 @@
 import pytest as pyt
 import numpy as np
+import math as ma
 from ParticleForUse import Particle as Pa 
+from ParticleAndSpring import ParticleAndSpring as PaS
+from PhysicalSystemAmend import PhysicalSystem as ps
 
 def test_update(): 
     TestP = Pa([0.0,0.0],[0.0,0.0],[1.0,1.0],'TestP')
@@ -16,3 +19,16 @@ def test_update_2():
     TestPECexp = Pa([1.0,1.0],[1.0,1.0],[1.0,1.0],'TestP')
 
     assert TestPEC == TestPECexp
+
+def test_acceleration():
+    TestObject1 = Pa([0,-1],[1,0],[0,0],'BO')
+    TestObject2 = PaS([0,0],[1,0],[0,0],'TO',1,10,0)
+
+    Test1 = ps(2,1,TestObject2,TestObject1)
+    Test1.Movement()
+
+    finaltestobject = Test1.TopObject.Acceleration[1]
+    dy = (TestObject2.Position[1] - TestObject1.Position[1])-1
+    finaltestexp = (1/(TestObject2.Mass))*(-(TestObject2.LinF)*dy)
+
+    assert finaltestobject == finaltestexp
